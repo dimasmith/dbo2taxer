6b427f419537cd5d22fec8105370c4476ec7eb26
Dmytro Kovalchuk <dimasmith@gmail.com>
2025-07-12 22:51:12 +0300

    feat: convert files from stdin

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # DBO2TAXER
 
 Simple cli utility to convert banking statements in DBO format to CSV format of [Taxer](https://taxer.ua).
+
+## Usage
+
+Pass dbo records in standard input and receive taxer operations in standard output.
+
+```bash
+cat dbo-statement.csv | dbo2taxer > taxer-operations.csv
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io::{stdin, stdout};
+
+use dbo_csv::{DboRecord, DboStatement, deserialize_statement};
+use taxer_csv::{TaxerRecord, serialize_taxer};
+
 fn main() {
-    println!("dbo2taxer. implementation pending");
+    let statement_input = stdin();
+    let statement = deserialize_statement(statement_input).unwrap();
+    let taxer_out = stdout();
+    let records = convert_records(statement);
+    serialize_taxer(taxer_out, &records).unwrap();
+}
+
+fn convert_records(statement: DboStatement) -> Vec<TaxerRecord> {
+    statement.iter().map(convert_record).collect()
+}
+
+fn convert_record(record: &DboRecord) -> TaxerRecord {
+    TaxerRecord::builder()
+        .tax_code_raw(record.party_tax_id.clone())
+        .unwrap()
+        .amount_raw(record.coverage)
+        .unwrap()
+        .date(record.operation_date)
+        .comment(record.payment_purpose.clone())
+        .build()
+        .unwrap()
 }
