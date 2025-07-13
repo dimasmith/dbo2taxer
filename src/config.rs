7f04c8cf939346b7d20261ff4af17567e59912be
Dmytro Kovalchuk <dimasmith@gmail.com>
2025-07-13 18:58:04 +0300

    feat: read configuration from the default config

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,4 @@
+use directories::ProjectDirs;
 use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
@@ -24,12 +25,20 @@ impl Default for TaxerConfig {
 impl TaxerConfig {
     pub fn from_configuration() -> anyhow::Result<Self> {
         let default_config = TaxerConfig::default();
-        let config = config::Config::builder()
+        let mut config = config::Config::builder()
             .set_default("operation", default_config.operation)?
-            .set_default("income_type", default_config.income_type)?
-            .add_source(config::Environment::with_prefix("DBO2TAXER"))
-            .build()?;
+            .set_default("income_type", default_config.income_type)?;
 
+        if let Some(project_dirs) = ProjectDirs::from("", "", "dbo2taxer") {
+            let config_dir = project_dirs.config_dir();
+            if let Some(dir_name) = config_dir.to_str() {
+                config = config.add_source(config::File::with_name(dir_name).required(false));
+            }
+        }
+
+        config = config.add_source(config::Environment::with_prefix("DBO2TAXER"));
+
+        let config = config.build()?;
         Ok(config.try_deserialize()?)
     }
 }
