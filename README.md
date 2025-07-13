577fd76ddbc18b2cb82b140653930584b825417e
Dmytro Kovalchuk <dimasmith@gmail.com>
2025-07-13 15:12:32 +0300

    feat: set additional properties with env

--- README.md
+++ README.md
@@ -9,3 +9,28 @@ Pass dbo records in standard input and receive taxer operations in standard outp
 ```bash
 cat dbo-statement.csv | dbo2taxer > taxer-operations.csv
 ```
+
+### Overriding additional values
+
+You can override a few additional values making Taxer format more complete.
+The values are:
+
+- Operation type
+- Income kind
+- Account name
+- Currency code
+
+Here's the example
+
+```bash
+cat dbo-statement.csv | DBO2TAXER_ACCOUNT_NAME="ФОП" dbo2taxer
+```
+
+The list of environment variables and default values.
+
+| Variable                | Default value    | Meaning                            |
+| ----------------------- | ---------------- | ---------------------------------- |
+| DBO2TAXER_ACCOUNT_NAME  | ""               | Name of account in the income list |
+| DBO2TAXER_OPERATION     | "Дохід"          | Type of operation                  |
+| DBO2TAXER_INCOME_TYPE   | "Основний дохід" | The type of income for taxation    |
+| DBO2TAXER_CURRENCY_CODE | "UAH"            | Income currency                    |
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
 use std::io::{stdin, stdout};
 
 use dbo_csv::{DboRecord, DboStatement, deserialize_statement};
+use dbo2taxer::TaxerConfig;
 use taxer_csv::{TaxerRecord, serialize_taxer};
 
 fn main() -> anyhow::Result<()> {
+    let taxer_config = TaxerConfig::from_configuration()?;
     let statement_input = stdin();
     let statement = deserialize_statement(statement_input)?;
     let taxer_out = stdout();
-    let records = convert_records(statement)?;
+    let records = convert_records(statement, &taxer_config)?;
     serialize_taxer(taxer_out, &records)?;
     Ok(())
 }
 
-fn convert_records(statement: DboStatement) -> anyhow::Result<Vec<TaxerRecord>> {
+fn convert_records(
+    statement: DboStatement,
+    config: &TaxerConfig,
+) -> anyhow::Result<Vec<TaxerRecord>> {
     let mut taxer_operations = vec![];
     for dbo in statement.into_inner() {
-        taxer_operations.push(convert_record(dbo)?);
+        taxer_operations.push(convert_record(dbo, config)?);
     }
     Ok(taxer_operations)
 }
 
-fn convert_record(record: DboRecord) -> anyhow::Result<TaxerRecord> {
+fn convert_record(record: DboRecord, config: &TaxerConfig) -> anyhow::Result<TaxerRecord> {
     TaxerRecord::builder()
         .tax_code_raw(record.party_tax_id)?
         .amount_raw(record.coverage)?
         .date(record.operation_date)
         .comment(record.payment_purpose)
+        .operation(config.operation.clone())
+        .income_type(config.income_type.clone())
+        .account_name(config.account_name.clone())
+        .currency_code(config.currency_code.clone())
         .build()
         .map_err(|err| anyhow::anyhow!(err))
 }
